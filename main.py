9057e02dcd78ad7cab79c046404a268f92cc1f9d
Tanner Watmough <79290989+tannerwatmough@users.noreply.github.com>
2025-09-02 19:47:14 -0600

    Added a dictionary sort funciton

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_chars
+from stats import *
 
 def get_book_text(book):
   with open(book) as f:
@@ -8,8 +8,18 @@ def get_book_text(book):
 def main():
   word_count = count_words(get_book_text("./books/frankenstein.txt"))
 
-  print(f'{word_count} words found in the document')
+  char_count_dict = count_chars(get_book_text("./books/frankenstein.txt"))
 
-  print(count_chars(get_book_text("./books/frankenstein.txt")))
+  sorted_dict_list = chars_dict_to_sorted_list(char_count_dict)
+
+  print("============ BOOKBOT ============")
+  print("Analyzing book found at books/frankenstein.txt...")
+  print("----------- Word Count ----------")
+  print(f'Found {word_count} total words')
+  print("--------- Character Count -------")
+  for item in sorted_dict_list:
+    if item["char"].isalpha():
+      print(f"{item['char']}: {item['num']}")
+  print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -10,3 +10,13 @@ def count_chars(book_string):
     else:
       char_count[c] = 1
   return char_count
+
+def sort_on(d):
+  return d["num"]
+
+def chars_dict_to_sorted_list(char_count_dict):
+  sorted_list = []
+  for c in char_count_dict:
+    sorted_list.append({"char": c, "num": char_count_dict[c]})
+  sorted_list.sort(reverse=True, key=sort_on)
+  return sorted_list
